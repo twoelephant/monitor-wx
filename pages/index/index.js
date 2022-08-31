57a9b3ef6d87ce2c29facf9bbec53447c27715f2
Your Name <you@example.com>
2022-08-31 20:54:54 +0800

    02

--- pages/index/index.js
+++ pages/index/index.js
@@ -3,16 +3,33 @@
 const app = getApp()
 
 Page({
-  data: {
-    
-   
-	
-	
-  },
- 
-  onLoad() {
-  
-  },
+	data: {
+		shopName: "店铺名称",
+		fit:false
+	},
 
-  
+	/* 判断是否是刘海屏 */
+	isIphoneX() {
+		let res = wx.getSystemInfoSync();
+		var safeBottom = res.screenHeight - res.safeArea.bottom
+		//根据安全高度判断
+		return  safeBottom === 34 ?true:false
+	   },
+	
+
+	onLoad() {
+		this.setData({
+			fit:this.isIphoneX()
+		}),
+		// console.log(this.data.fit)
+		wx.setNavigationBarTitle({
+			/* 动态设置NavigationBarTitle */
+			title: this.data.shopName,
+		})
+		// console.log(this.isIphoneX())
+		
+
+	},
+
+
 })
