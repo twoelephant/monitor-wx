0c642de5c2f1ee4224f6df3529d0d117ef3e8051
aaa <aaa@example.com>
2022-09-02 09:24:16 +0800

    v0.0.12

--- pages/my/my.js
+++ pages/my/my.js
@@ -5,10 +5,19 @@ Page({
    * 页面的初始数据
    */
   data: {
-    
       nbFrontColor: '#000000',
       nbBackgroundColor: '#ffffff',
-    
+      imglist:[
+        {src:'../image/优惠券.png',text:'领券中心'},
+        {src:'../image/权益.png',text:'权益卡'},
+        {src:'../image/自助充值.png',text:'自动充值'},
+        {src:'../image/无人自助.png',text:'无人自助'},
+        {src:'../image/收货地址.png',text:'收货地址'},
+        {src:'../image/分销员.png',text:'分销员'},
+        {src:'../image/线下密码.png',text:'线下密码'},
+        {src:'../image/附近.png',text:'附近门店'}
+      ]
+  
   },
 
   /**
